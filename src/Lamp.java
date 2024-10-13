a626f58aff0c0b1ad593b4369bb42a170e12e3fb
Habeeb <habeeebkinlolu234@.com>
2024-10-13 16:44:27 +0100

    initialized

---
+++ src/Lamp.java
@@ -0,0 +1,24 @@
+public class Lamp {
+    private final String style;
+    private final boolean battery;
+    private final int globRating;
+
+    public Lamp(String style, boolean battery, int globRating) {
+        this.style = style;
+        this.battery = battery;
+        this.globRating = globRating;
+    }
+    public void turnOn() {
+        System.out.println("Lamp -> Turning on");
+    }
+    public String getStyle(){
+        return style;
+    }
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public int getGlobRating() {
+        return globRating;
+    }
+}
---
+++ src/Wall.java
@@ -0,0 +1,11 @@
+public class Wall {
+    private final String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
